e90dcca696889730ccfbf40d89967768945a0fe7
i-i-ijul <haqqikamvret@gmail.com>
2025-05-17 15:56:48 +0700

    menampilkan semua data buku

---
+++ tugas.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+class Judul {
+private:
+    string nama;
+
+public:
+    Judul(string n){
+    nama = n;
+    }
+  // konstruktor
+
+    friend class Petugas; // ngejadiin petugas sbg fri& class agar bisa memodifikasi
+};
+
+class Penulis {
+private:
+    string namaPenulis;
+
+public:
+    Penulis(string n){
+    namaPenulis = n;
+    }
+    // konstruktor
+
+    friend class Petugas; // ngejadiin petugas sbg fri& class agar bisa memodifikasi
+};
+
+class StatusPeminjaman {
+private:
+    bool dipinjam;
+
+public:
+    StatusPeminjaman(bool s){
+        dipinjam = s;
+    }
+    //konstruktor
+
+    friend class Petugas; // ngejadiin petugas sbg fri& class agar bisa memodifikasi
+};
+
+class Petugas {
+public:
+    void tampilData(Judul j, Penulis p, StatusPeminjaman s){
+        cout << "JUDUL BUKU: " << j.nama << endl;
+        cout << "NAMA PENULIS: " << p.namaPenulis << endl;
+        cout << "STATUS: " << "dipinjam" << endl;
+        cin >> s.dipinjam;
+    }
+};
+
+int main() {
+    Judul j("IJUL GANTENG");
+    Penulis p("haqqi");
+    StatusPeminjaman s("dipinjam");
+
+    Petugas petugas;
+    petugas.tampilData(j, p, s);
+
+    return 0;
+}
